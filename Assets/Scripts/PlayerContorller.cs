628706bcab9c93e5a75eaf27ba1cdd74cbf984d3
Samretwit Damri <damri.samretwit@gmail.com>
2017-03-12 15:26:18 +0900

    UnityRun １回目コミット

---
+++ Assets/Scripts/PlayerContorller.cs
@@ -0,0 +1,150 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerContorller : MonoBehaviour {
+
+	private Vector2 startPos;
+	private Vector2 direction;
+	private Vector2 endPos;
+
+	private float clickTime;
+	private float distance;
+	private float angle;
+
+	private bool isClick = false;
+	private bool isDirectClick = false;
+	private bool isTabToLeft = false;
+	private bool isTabToRight = false;
+	private bool isTabToTop = false;
+	private bool isTabToBottom = false;
+
+	// Use this for initialization
+	void Start () {
+		clickTime = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (true) {
+			if (Input.GetMouseButtonDown (0))
+				BeginClick ();
+			if (Input.GetMouseButtonUp (0))
+				EndClick ();
+			if (Input.GetMouseButton (0))
+				TrackingClick ();
+		}
+	}
+
+	void clickTimeout()
+	{
+		isDirectClick = false;
+		isTabToLeft = false;
+		isTabToRight = false;
+		isTabToTop = false;
+		isTabToBottom = false;
+	}
+
+	private void BeginClick(){
+		startPos = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+		//Debug.Log ("startPos :" + startPos.x + " " + startPos.y);
+	}
+
+	private void EndClick(){
+		endPos = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+
+		if (clickTime <= 0.5f) {
+			isClick = true;
+		}
+
+		if (isClick && (distance <= 1.5f))
+			isDirectClick = true;
+		
+		if (isClick && (-45f < angle && angle < 45f))
+			isTabToLeft = true;
+
+		if (isClick && ((angle > 135f || angle < -135f) && angle != 360.0f))
+			isTabToRight = true;
+
+		if (isClick && (45f < angle && angle < 135f))
+			isTabToTop = true;
+
+		if (isClick && (-135f < angle && angle < -45))
+			isTabToBottom = true;
+
+		//値リセット
+		clickTime = 0.0f;
+		isClick = false;
+
+		Invoke ("clickTimeout", 0.1f);
+	}
+
+	private void TrackingClick(){
+		distance = 0.0f;
+		angle = 360.0f;
+		clickTime += Time.deltaTime;
+		Vector2 currentPos = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+
+		distance = Vector2.Distance (currentPos, startPos) / 50.0f;
+		if (distance >= 1.5f) {
+			direction = currentPos - startPos;
+			float sign = (currentPos.y < startPos.y) ? -1.0f : 1.0f;
+			angle = (Vector2.Angle (Vector2.left, direction) * sign);
+			//Debug.Log (angle);
+		}
+	}
+
+	public bool IsClick()
+	{
+		return isClick;
+	}
+
+	public bool IsDirectClick()
+	{
+		if (isDirectClick) {
+			isDirectClick = false;
+			return true;
+		}
+		else
+			return false;
+	}
+
+	public bool IsTabToLeft()
+	{
+		if (isTabToLeft) {
+			isTabToLeft = false;
+			return true;
+		}
+		else
+			return false;
+	}
+
+	public bool IsTabToRight()
+	{
+		if (isTabToRight) {
+			isTabToRight = false;
+			return true;
+		}
+		else
+			return false;
+	}
+
+	public bool IsTabToTop()
+	{
+		if (isTabToTop) {
+			isTabToTop = false;
+			return true;
+		}
+		else
+			return false;
+	}
+
+	public bool IsTabToBottom()
+	{
+		if (isTabToBottom) {
+			isTabToBottom = false;
+			return true;
+		}
+		else
+			return false;
+	}
+}
---
+++ Assets/Scripts/UnityChanController.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityChanController : MonoBehaviour {
+
+	public float Speed = 5f;
+
+	public float SpeedRatio = 1f;
+
+	public float jumpForce = 300f;
+
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.1f;
+	public LayerMask whatIsGround;
+
+	bool doubleJump = false;
+
+	private Rigidbody myRigitbody;
+
+	private Animator myAnimator;
+
+	private PlayerContorller myPlayerController;
+
+	public GameObject myPuniCon;
+
+
+	// Use this for initialization
+	void Awake () {
+		myRigitbody = GetComponent<Rigidbody> ();
+		myAnimator = GetComponent<Animator> ();
+		myPlayerController = myPuniCon.GetComponent<PlayerContorller> ();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		grounded = Physics.Raycast(groundCheck.position, -Vector3.up, groundRadius);
+		myAnimator.SetBool ("IsGround", grounded);
+
+		if (grounded)
+			doubleJump = false;
+
+		myAnimator.SetFloat ("vSpeed", myRigitbody.velocity.y);
+
+		Vector3 movement = transform.forward * Speed * SpeedRatio * Time.deltaTime;
+		this.myRigitbody.MovePosition (myRigitbody.position + movement);
+		this.myRigitbody.rotation = Quaternion.Euler(transform.rotation.x, 90, transform.rotation.z);
+
+		if (SpeedRatio >= 2)
+			this.myAnimator.SetFloat ("Speed", 2);
+		else if (SpeedRatio >= 1)
+			this.myAnimator.SetFloat ("Speed", 1);
+		else
+			this.myAnimator.SetFloat ("Speed", 0);
+	}
+
+	void Update()
+	{
+		if ((grounded || !doubleJump) && myPlayerController.IsDirectClick()) {
+			myAnimator.SetBool ("IsGround", false);
+			myRigitbody.velocity = new Vector3 (0, 0, 0);
+			myRigitbody.AddForce (new Vector3 (0, jumpForce, 0));
+
+			if (!doubleJump && !grounded)
+				doubleJump = true;
+		}
+
+		if (myPlayerController.IsTabToRight ())
+			SpeedRatio = 2;
+		else if (myPlayerController.IsTabToLeft ())
+			SpeedRatio = 1;
+
+		if (myPlayerController.IsTabToBottom ()) {
+			myRigitbody.velocity = new Vector3 (0, 0, 0);
+			myRigitbody.AddForce (new Vector3 (0, -jumpForce * 2.0f, 0));
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		if (other.gameObject.tag == "CoinTag") {
+
+			//パーティクルを再生（追加）
+			GetComponent<ParticleSystem> ().Play ();
+
+			//接触したコインのオブジェクトを破棄（追加）
+			Destroy (other.gameObject);
+		}
+	}
+}
